963e6ad9f04b52dca6108921c6c697d0ce873edc
Alicenj <128193709+Alicenj@users.noreply.github.com>
2023-05-22 06:02:57 +0300

    Комментарий

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,13 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println("Равны ли по ссылке? " + (bean1 == bean2));
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Равны ли по ссылке? " + (cat1 == cat2));
+        Cat cat3 = (Cat) applicationContext.getBean("cat");
+        System.out.println("Равны ли по ссылке? " + (cat1 == cat3));
     }
 }
